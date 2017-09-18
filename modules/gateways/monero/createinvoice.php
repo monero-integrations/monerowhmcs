9778d0d56860ab59c1f2a7dc5207e2fe1e128f59
serhack <atombt@protonmail.com>
2017-09-18 15:04:12 +0200

    Update createinvoice.php

--- modules/gateways/monero/createinvoice.php
+++ modules/gateways/monero/createinvoice.php
@@ -28,4 +28,34 @@ echo "<div class='alert alert-warning' id='message'>".$message."</div>";
 echo "<div class='row'>
  <div class='col-sm-12 col-md-12 col-lg-12'>                      
  <div class='panel panel-default' id='PaymentBox_de3a227fb470475'>
+ <div class='panel-body'>
+ <div class='row'>
+                  <div class='col-sm-12 col-md-12 col-lg-12'>
+                              <h3> Monero Payment Box</h3>
+                  </div>
+                     <div class='col-sm-3 col-md-3 col-lg-3'>
+ <img src='https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=" . $uri ."' class='img-responsive'>
+                         </div>
+          <div class='col-sm-9 col-md-9 col-lg-9' style='padding:10px;'>
+    Send <b>".$amount."  XMR</b> to<br/><input type='text'  class='form-control' value='" . $array_integrated_address['integrated_address']."'>
+    or scan QR Code with your mobile device<br/><br/>
+    <small>If you don't know how to pay with monero or you don't know what monero is, please go <a href='#'>here</a>. </small>
+    </div>
+    <div class='col-sm-12 col-md-12 col-lg-12'>
+        </div>
+                                                </div>
+                                                 </div>
+                                   
+                              </div>
+                    </div>
+                </div>
+</div>";
+echo "<script> function verify(){ 
+$.ajax({ url : 'verify.php',
+type : 'POST', 
+data: { 'amount' : '".$amount."', 'payment_id' : '".$payment_id."', 'invoice_id' : '".$invoice_id."'}, 
+error: function(data){ console.log(data); } 
+success: function(msg){ console.log(msg); $('#message').text(msg); }  }); } 
+verify(); setInterval(function(){ verify()}, 5000);</script>";
+?>
 
